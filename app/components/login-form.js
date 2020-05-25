b9525669f7c9e2e27d3ab00a137742977b7d1e82
Yves Desjardins <YvesDesjardins@gmail.com>
2020-05-25 16:34:48 -0700

    step 8

--- app/components/login-form.js
+++ app/components/login-form.js
@@ -1,14 +1,31 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 
 export default class LoginFormComponent extends Component {
+  @tracked
+  userId = null;
+
+  get isDisabled() {
+    return !this.userId;
+  }
+
   loginAsUserWithId(val) {
     console.log('UserId:', val)
   }
 
   /**
-   * 
-   * @param {Event & {target: HTMLFormElement}} event 
+   *
+   * @param {Event & { target: HTMLSelectElement }} event
+   */
+  @action
+  onSelectChanged(event) {
+    this.userId = event.target.value;
+  }
+
+  /**
+   *
+   * @param {Event & {target: HTMLFormElement}} event
    */
 
   @action
