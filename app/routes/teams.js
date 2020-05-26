f3551dffd522a759b9c970f09e19e4c592ecc031
Yves Desjardins <YvesDesjardins@gmail.com>
2020-05-26 14:27:47 -0700

    variable teams list

--- app/routes/teams.js
+++ app/routes/teams.js
@@ -1,12 +1,33 @@
 import Route from '@ember/routing/route';
-import {inject as service} from '@ember/service';
+import { inject as service } from '@ember/service';
 import AuthService from 'shlack/services/auth';
+
+export const ALL_TEAMS = [
+  {
+    id: 'linkedin',
+    name: 'LinkedIn',
+    order: 2,
+    iconUrl: '/assets/img/linkedin.png',
+  },
+  {
+    id: 'ms',
+    name: 'Microsoft',
+    order: 3,
+    iconUrl: '/assets/img/microsoft.png',
+  },
+  {
+    id: 'avengers',
+    name: 'Avengers',
+    order: 4,
+    iconUrl: '/assets/img/avengers.jpg',
+  },
+];
 
 export default class TeamsRoute extends Route {
   /**
    * @type {AuthService}
    */
-  @service auth
+  @service auth;
 
   async beforeModel(transition) {
     await super.beforeModel(transition);
@@ -15,5 +36,7 @@ export default class TeamsRoute extends Route {
     }
   }
 
-  model() {}
+  async model() {
+    return ALL_TEAMS;
+  }
 }
