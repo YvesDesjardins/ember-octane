fb0349a3514bb80bc448c7eac7e8e1a20b8310d2
Yves Desjardins <YvesDesjardins@gmail.com>
2020-05-26 17:05:29 -0700

    message submission implemented

--- app/components/chat-container.js
+++ app/components/chat-container.js
@@ -2,10 +2,17 @@ import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 import fetch from 'fetch';
+import { inject as service } from '@ember/service';
+import AuthService from 'shlack/services/auth';
 
 export default class ChatContainerComponent extends Component {
   @tracked
   messages = [];
+
+  /**
+   * @type {AuthService}
+   */
+  @service auth;
 
   @action
   async loadMessages() {
@@ -18,5 +25,27 @@ export default class ChatContainerComponent extends Component {
     );
     // this.messages = [...this.messages, ...(await response.json())];
     this.messages = await response.json();
+  }
+
+  @action
+  async createMessage(body) {
+    const {
+      channel: { id: channelId, teamId },
+    } = this.args;
+
+    const userId = this.auth.currentUserId;
+
+    const response = await fetch('/api/messages', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/JSON',
+      },
+      body: JSON.stringify({
+        teamId,
+        channelId,
+        userId,
+        body,
+      }),
+    });
   }
 }
